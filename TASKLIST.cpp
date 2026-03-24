526337dd531c13d1c7f2cbc31f0764a52213688b
FlanMan04 <clubgus2004@gmail.com>
2026-03-24 04:19:14 -0400

    added stdexcept, finished logic for remove/peek/ searchTask

--- TASKLIST.cpp
+++ TASKLIST.cpp
@@ -1,5 +1,5 @@
 #include "TASKLIST.h"
-
+#include <stdexcept>
 TaskList::TaskList() {}
 TaskList::TaskList(const TaskList& other) {
     head = nullptr;
@@ -46,15 +46,71 @@ TaskList::TaskList(const TaskList& other) {
     }
 
     Task TaskList::removeTask() {
+        if (head == nullptr) {
+            throw std::underflow_error("List is empty");
+        }
 
+        if (head->next == nullptr){
+            Task removedTask = head->data;
+            delete head;
+            head = nullptr;
+            
+            size--;
+            
+            return removedTask;
+        }
+
+        Node* ptr = head;
+        while(ptr->next->next != nullptr){
+            ptr = ptr->next;
+        }
+        Task removedTask= ptr->next->data;
+        delete ptr->next;
+        ptr->next = nullptr;
+
+        size--;
+        
+        return removedTask;
     }
 
     Task TaskList::peekTask() {
+        if (head == nullptr) {
+            throw std::underflow_error("List is empty");
+        }
 
+        Node* ptr = head;
+        while(ptr->next != nullptr) {
+            ptr = ptr->next;
+        }
+
+        return ptr->data;
     }
 
-    bool TaskList::searchTask(const Task &ID) {
+    void TaskList::showList() {
+        if (head == nullptr) {
+            throw std::underflow_error("List is empty");
+        }
 
+        Node* ptr = head;
+        while (ptr != nullptr) {
+            ptr->data.show();
+            ptr = ptr->next;
+        }
+    }
+
+    bool TaskList::searchTask(Task &t) {
+        if (head == nullptr) {
+            throw std::underflow_error("List is empty");
+        }
+        
+        Node* ptr = head;
+        while (ptr != nullptr) {
+            if(ptr->data.getID() == t.getID()) {
+                return true;
+            }
+            ptr = ptr->next;
+        }
+        return false;
     }
 
     void TaskList::setSize(int Size) {
